9a5e34e0aa18a571790699daa45227320faef973
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2018-04-12 15:08:45 +0200

    test: update test to match new feat (95beab2)

--- test.js
+++ test.js
@@ -11,11 +11,13 @@ app.listen(3000, function () {
 
   console.log("*** Start trackr ***")
 
+  const nodeUrl = "ws://localhost:8546"
+
   const addressToTrack = "0x40C76cf136c270c2F1053398f1718b3fFe2574c4"
 
-  console.log("\ttrack : "+addressToTrack)
+  console.log("\ttrack : "+addressToTrack+ " from node @ "+nodeUrl)
 
-  trackr.watch(addressToTrack,(error,tx)=>{
+  trackr.watch(nodeUrl,addressToTrack,(error,tx)=>{
     if( !error ) console.log("--> new tx ("+tx+") with "+addressToTrack)
     else console.log(error)
   })
