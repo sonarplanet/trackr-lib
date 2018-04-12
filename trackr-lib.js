95beab2a35f2dd475d3e6401bccdf75f9f2c4d31
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2018-04-12 15:07:45 +0200

    feat: ethereum node to fetch is now set by user

--- trackr-lib.js
+++ trackr-lib.js
@@ -2,16 +2,17 @@ var exports = module.exports = {}
 
 const Web3 = require('web3-eth')
 
-var web3 = new Web3("ws://localhost:8546");
-
 /**
  * Listen new transactions and return its hash when address occurs in 'from' or 'to' property.
  * Start listening from latest block. 
  * Address matching is case insensitive.
+ * @param {string} nodeUrl url of the Ethereum node to communicate with
  * @param {string} address an Ethereum address to watch
  * @param {function} callback function called when event occurs. First argument is error and second is transaction hash
  */
-exports.watch = (address,callback) => {
+exports.watch = (nodeUrl,address,callback) => {
+
+    var web3 = new Web3(nodeUrl)
 
     web3.subscribe('newBlockHeaders',(error,_)=> { if( error ) callback(error) })
         .on('data', (newBlock)=>{
