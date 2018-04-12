d4ef258d7880b90fda8329dea05f6bdb850e4aa9
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2018-04-12 16:41:23 +0200

    style: remove useless code and syntacticc sugar from test

--- test.js
+++ test.js
@@ -3,11 +3,7 @@ const trackr = require('./trackr-lib')
 const express = require('express')
 const app = express()
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
-app.listen(3000, function () {
+app.listen(3000, ()=>{
 
   console.log("*** Start trackr ***")
 
