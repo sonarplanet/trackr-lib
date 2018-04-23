31d309338f47afa07ea3f7970703775c582cf3b2
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2018-04-23 10:01:41 +0200

    refactor: set type to some variables

--- src/test/test.ts
+++ src/test/test.ts
@@ -8,15 +8,15 @@ app.listen(3000, ()=>{
 
   console.log("*** Start trackr ***")
 
-  const nodeUrl = "wss://sonarplanet-eth-node.cleverapps.io/"
+  const nodeUrl : string = "wss://sonarplanet-eth-node.cleverapps.io/"
 
-  const addressToTrack = "0x40C76cf136c270c2F1053398f1718b3fFe2574c4"
+  const addressToTrack : string = "0x40C76cf136c270c2F1053398f1718b3fFe2574c4"
 
   console.log("\ttrack : "+addressToTrack+ " from node @ "+nodeUrl)
 
   trackr.watch(
     nodeUrl,addressToTrack,
-    (tx)=> console.log("--> new tx ("+tx+") with "+addressToTrack),
-    (error)=>console.log("an error occurs : "+JSON.stringify(error)))
+    (tx:string)=> console.log("--> new tx ("+tx+") with "+addressToTrack),
+    (error:Error)=>console.log("an error occurs : "+JSON.stringify(error)))
 
 })
