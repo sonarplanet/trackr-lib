a4b8ac3318ca4bcf78a6271aace5eadad60d5cd2
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2018-05-03 16:11:45 +0200

    test: refactor code and create tests for utilities

--- src/trackr-lib.ts
+++ src/trackr-lib.ts
@@ -1,4 +1,5 @@
-const Web3 = require('web3')
+const Web3 = require("web3");
+import * as Utilities from "./utilities";
 
 /**
  * Listen new transactions and return its hash when address occurs in 'from' or 'to' property.
@@ -15,21 +16,16 @@ export function watch(
   newTxCallback: (tx: string) => void,
   errorCallback: (error: Error) => void
 ): void {
-  const web3 = new Web3(nodeUrl).eth
-
-  const transactionAreEquals = (transactionAddress: string, currentAddress: string) => {
-    return transactionAddress != null && transactionAddress.toLowerCase() === currentAddress.toLowerCase()
-  }
+  const web3 = new Web3(nodeUrl).eth;
 
   web3
-    .subscribe('newBlockHeaders')
-    .on('data', (blockHeader: any) => {
+    .subscribe("newBlockHeaders")
+    .on("data", (blockHeader: any) => {
       web3.getBlock(blockHeader.number, true).then((block: any) => {
-        block.transactions.filter((transaction: any) => {
-          return transactionAreEquals(transaction.from, address)
-            || transactionAreEquals(transaction.to, address)
-        }).forEach((transaction: any) => newTxCallback(transaction.hash));
-      })
+        Utilities.findAddressInBlock(address, block).forEach(
+          (transactionHash: string) => newTxCallback(transactionHash)
+        );
+      });
     })
-    .on('error', errorCallback)
+    .on("error", errorCallback);
 }
