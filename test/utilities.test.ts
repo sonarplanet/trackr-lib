9b9c5559fe7eb5a90c2a07365a01378d97d0fcfc
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2018-05-07 16:35:28 +0200

    test: create test suite for refactor #106ecdc

--- test/utilities.test.ts
+++ test/utilities.test.ts
@@ -2,84 +2,73 @@
 
 import * as Utilities from '../src/utilities';
 
+let undefVar;
+const VALID_ADDRESS = '0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d';
+const INVALID_ADDRESS_SHORT = '0x612D7A97AFFc491e814a7d64638511280';
+const TRANSACTION_1 = { hash: '1', from: VALID_ADDRESS, to: VALID_ADDRESS };
+const TRANSACTION_2 = { hash: '2', from: '', to: VALID_ADDRESS };
+const TRANSACTION_3 = { hash: '3', from: '', to: undefVar };
+const TRANSACTION_4 = { hash: '4', from: INVALID_ADDRESS_SHORT };
+
+const A_BLOCK = {
+  transactions: [TRANSACTION_1, TRANSACTION_2, TRANSACTION_3, TRANSACTION_4],
+};
+
 describe('test addressesAreEquals function', () => {
   it('addressesAreEquals', () => {
-    expect(
-      Utilities.addressesAreEquals(
-        '0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d',
-        '0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d',
-      ),
-    ).toEqual(true);
+    expect(Utilities.addressesAreEquals(VALID_ADDRESS, VALID_ADDRESS)).toEqual(true);
 
-    expect(
-      Utilities.addressesAreEquals(
-        '0x612D7A97AFFc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d',
-        '0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d',
-      ),
-    ).toEqual(true);
+    expect(Utilities.addressesAreEquals(VALID_ADDRESS, VALID_ADDRESS)).toEqual(true);
 
-    expect(
-      Utilities.addressesAreEquals(
-        '0x612D7A97AFFc491e814a7d64638511280',
-        '0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d',
-      ),
-    ).toEqual(false);
+    expect(Utilities.addressesAreEquals(INVALID_ADDRESS_SHORT, VALID_ADDRESS)).toEqual(false);
 
-    expect(
-      Utilities.addressesAreEquals(
-        '0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d',
-        '0x612D7A97AFFc491e814a7d64638511280',
-      ),
-    ).toEqual(false);
+    expect(Utilities.addressesAreEquals(VALID_ADDRESS, INVALID_ADDRESS_SHORT)).toEqual(false);
 
-    expect(
-      Utilities.addressesAreEquals(null, '0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d'),
-    ).toEqual(false);
+    expect(Utilities.addressesAreEquals(null, VALID_ADDRESS)).toEqual(false);
 
-    expect(
-      Utilities.addressesAreEquals('0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d', null),
-    ).toEqual(false);
+    expect(Utilities.addressesAreEquals(VALID_ADDRESS, null)).toEqual(false);
 
-    let undefVar;
+    expect(Utilities.addressesAreEquals(VALID_ADDRESS, undefVar)).toEqual(false);
 
-    expect(
-      Utilities.addressesAreEquals('0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d', undefVar),
-    ).toEqual(false);
-
-    expect(
-      Utilities.addressesAreEquals(undefVar, '0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d'),
-    ).toEqual(false);
+    expect(Utilities.addressesAreEquals(undefVar, VALID_ADDRESS)).toEqual(false);
   });
 });
 
-describe('test findAddressInBlock function', () => {
-  it('findAddressInBlock', () => {
-    const addressToFind: string = '0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d';
+describe('test filterAddressesAreEqualsTo function', () => {
+  it('filterAddressesAreEqualsTo', () => {
+    expect(
+      [VALID_ADDRESS, INVALID_ADDRESS_SHORT, undefVar, VALID_ADDRESS].filter(
+        Utilities.filterAddressesAreEqualsTo(VALID_ADDRESS),
+      ),
+    ).toEqual([VALID_ADDRESS, VALID_ADDRESS]);
 
-    const block = {
-      transactions: [
-        {
-          hash: '1',
-          from: '0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d',
-          to: '0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d',
-        },
-        {
-          hash: '2',
-          from: '',
-          to: '0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d',
-        },
-        {
-          hash: '3',
-          from: '',
-          to: '',
-        },
-        {
-          hash: '4',
-          from: '0x612D7A97AFFc491e814a7d64638511280',
-        },
-      ],
+    expect([INVALID_ADDRESS_SHORT, undefVar].filter(Utilities.filterAddressesAreEqualsTo(VALID_ADDRESS))).toEqual([]);
+  });
+});
+
+describe('test filterTransactionsUsingAddress function', () => {
+  it('filterTransactionsUsingAddress', () => {
+    let input = A_BLOCK.transactions;
+    let expected = [TRANSACTION_1, TRANSACTION_2];
+    let filter = Utilities.filterTransactionsUsingAddress(VALID_ADDRESS);
+    expect(A_BLOCK.transactions.filter(filter)).toEqual(expected);
+  });
+});
+
+describe('test applyOnFilteredBlockTransactions function', () => {
+  it('applyOnFilteredBlockTransactions', (done) => {
+    const EXPECTED_CALLBACK_RESULT: string[] = ['1', '2'];
+
+    let hashes: string[] = [];
+    let filter = Utilities.filterTransactionsUsingAddress(VALID_ADDRESS);
+    let callback = async (hash: string) => {
+      hashes.push(hash);
+      if (hashes.length == EXPECTED_CALLBACK_RESULT.length) {
+        expect(hashes).toEqual(EXPECTED_CALLBACK_RESULT);
+        done();
+      }
     };
 
-    expect(Utilities.findAddressInBlock(addressToFind, block)).toEqual(['1', '2']);
+    Utilities.applyOnFilteredBlockTransactions(filter, callback)(A_BLOCK);
   });
 });
