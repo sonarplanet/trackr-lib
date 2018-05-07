8fff64ca55cdc563f1c23406f22e091e4e386cf9
Guillaume Nicolas <guillaume.nicolas@spacelephant.org>
2018-05-07 13:41:37 +0200

    test: replace toBeTrusty/Falsy by toEqual(true/false)

--- test/utilities.test.ts
+++ test/utilities.test.ts
@@ -1,67 +1,85 @@
 /// <reference types="jest" />
 
-import * as Utilities from "../src/utilities";
+import * as Utilities from '../src/utilities';
 
-describe("test addressesAreEquals function", () => {
-  it("addressesAreEquals", () => {
+describe('test addressesAreEquals function', () => {
+  it('addressesAreEquals', () => {
     expect(
       Utilities.addressesAreEquals(
-        "0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d",
-        "0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d"
-      )
-    ).toBeTruthy();
-
-    expect(
-      Utilities.addressesAreEquals(
-        "0x612D7A97AFFc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d",
-        "0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d"
-      )
-    ).toBeTruthy();
+        '0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d',
+        '0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d',
+      ),
+    ).toEqual(true);
 
     expect(
       Utilities.addressesAreEquals(
-        "0x612D7A97AFFc491e814a7d64638511280",
-        "0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d"
-      )
-    ).toBeFalsy();
+        '0x612D7A97AFFc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d',
+        '0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d',
+      ),
+    ).toEqual(true);
+
+    expect(
+      Utilities.addressesAreEquals(
+        '0x612D7A97AFFc491e814a7d64638511280',
+        '0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d',
+      ),
+    ).toEqual(false);
+
+    expect(
+      Utilities.addressesAreEquals(
+        '0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d',
+        '0x612D7A97AFFc491e814a7d64638511280',
+      ),
+    ).toEqual(false);
+
+    expect(
+      Utilities.addressesAreEquals(null, '0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d'),
+    ).toEqual(false);
+
+    expect(
+      Utilities.addressesAreEquals('0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d', null),
+    ).toEqual(false);
+
+    let undefVar;
+
+    expect(
+      Utilities.addressesAreEquals('0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d', undefVar),
+    ).toEqual(false);
+
+    expect(
+      Utilities.addressesAreEquals(undefVar, '0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d'),
+    ).toEqual(false);
   });
 });
 
-describe("test findAddressInBlock function", () => {
-  it("findAddressInBlock", () => {
-    const addressToFind: string =
-      "0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d";
+describe('test findAddressInBlock function', () => {
+  it('findAddressInBlock', () => {
+    const addressToFind: string = '0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d';
 
     const block = {
       transactions: [
         {
-          hash: "1",
-          from:
-            "0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d",
-          to:
-            "0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d"
+          hash: '1',
+          from: '0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d',
+          to: '0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d',
         },
         {
-          hash: "2",
-          from: "",
-          to:
-            "0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d"
+          hash: '2',
+          from: '',
+          to: '0x612d7a97affc491e814a7d64638511280a1b395243626d0a94e34cf8cc495e7d',
         },
         {
-          hash: "3",
-          from: "",
-          to: ""
+          hash: '3',
+          from: '',
+          to: '',
         },
         {
-          hash: "4",
-          from: "0x612D7A97AFFc491e814a7d64638511280"
-        }
-      ]
+          hash: '4',
+          from: '0x612D7A97AFFc491e814a7d64638511280',
+        },
+      ],
     };
 
-    expect(Utilities.findAddressInBlock(addressToFind, block)).toEqual([
-      "1",
-      "2"
-    ]);
+    expect(Utilities.findAddressInBlock(addressToFind, block)).toEqual(['1', '2']);
   });
 });
